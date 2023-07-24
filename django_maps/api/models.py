3f995361ae4b3267f411499374e4980e937be567
Huzaifa <saadhuazifa2497@gmail.com>
2023-07-24 07:30:05 +0600

    api model setup

--- django_maps/api/models.py
+++ django_maps/api/models.py
@@ -1,3 +1,8 @@
 from django.db import models
-
+from django.db.models import FloatField,CharField
 # Create your models here.
+class Location(models.Model):
+    lng=FloatField(null=True,blank=True)
+    lat=FloatField(null=True,blank=True)
+    name=CharField(null=True,blank=True)
+    
--- django_maps/django_maps/settings.py
+++ django_maps/django_maps/settings.py
@@ -42,7 +42,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'one'
+    'rest_framework',
+    'api',
+    'one',
+    
 ]
 
 MIDDLEWARE = [
